38d1ebb47a666a298adb63d57732dd31c1b02a0b
Soemsak Loetphornphisit <soemsakplus@gmail.com>
2018-02-12 16:05:26 +0700

    Breeze - Save

--- idlikeanimation/src/main/java/com/moohow/idlikeanimation/Model/IDUser.kt
+++ idlikeanimation/src/main/java/com/moohow/idlikeanimation/Model/IDUser.kt
@@ -1,7 +1,20 @@
 package com.moohow.idlikeanimation.Model
 
+interface xxxxx {
+    fun onHoldStart()
+    fun onHoldRelease(): Int
+}
+
 class IDUser {
     var firsname: String? = null
     var lastname: String? = null
     var age: Int = 0
+
+    var listener: xxxxx? = null
+
+    fun initButton(percent: Int){
+        if(listener != null){
+            listener!!.onHoldRelease()
+        }
+    }
 }
